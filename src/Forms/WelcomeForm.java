eb7fd3e0992a0eb1c93fbbd1db97780bf8558111
DusanVukovic3 <vukovic.ra16.2019@uns.ac.rs>
2023-12-11 14:41:39 +0100

    radi uzimanje svih mejlova iz inboxa koji nisu procitani, mogu i da namestim datum od kog zelim da uzimam mejlove

--- src/Forms/WelcomeForm.java
+++ src/Forms/WelcomeForm.java
@@ -6,6 +6,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class WelcomeForm extends JFrame {
+    private final EmailManager emailManager;
+    private JButton checkEmailButton;
+    private JButton stopEmailButton;
+    private SwingWorker<Void, String> emailRetrievalWorker;
+
     public WelcomeForm(JFrame parent, User user) {
         super("Welcome Form");
         setLayout(new FlowLayout());
@@ -14,20 +19,52 @@ public class WelcomeForm extends JFrame {
         JLabel welcomeLabel = new JLabel("Welcome, " + user.getEmail() + "!");
         add(welcomeLabel);
 
-        JButton checkEmailButton = new JButton("Check New Emails");
+        checkEmailButton = new JButton("Check New Emails");
         checkEmailButton.addActionListener(e -> checkNewEmails(user));
         add(checkEmailButton);
+
+        stopEmailButton = new JButton("Stop Retrieval");
+        stopEmailButton.addActionListener(e -> stopEmailRetrieval());
+        add(stopEmailButton);
 
         pack();
         setMinimumSize(new Dimension(400, 500));
         setLocationRelativeTo(parent);
         setVisible(true);
+
+        // Create EmailManager instance
+        emailManager = new EmailManager();
     }
 
     private void checkNewEmails(User user) {
-        System.out.println("Checking new emails...");
-        EmailManager emailManager = new EmailManager(user.getEmail(), user.getPassword());
+        checkEmailButton.setEnabled(false);
+        stopEmailButton.setEnabled(true);
 
-        System.out.println("Email check completed.");
+        emailManager.resetStopRetrieval();
+        emailRetrievalWorker = new SwingWorker<Void, String>() {  //Novi swingWorker svaki put kada se dugme checkEmails klikne
+            @Override
+            protected Void doInBackground() throws Exception {
+                System.out.println("Checking new emails...");
+                emailManager.handleEmailRetrieval("imap.gmail.com", user.getEmail(), user.getPassword());
+                return null;
+            }
+            @Override
+            protected void done() {
+                checkEmailButton.setEnabled(true);
+                stopEmailButton.setEnabled(false);
+            }
+        };
+
+        emailRetrievalWorker.execute();
     }
+
+    private void stopEmailRetrieval() {
+        System.out.println("Stopping email retrieval...");
+        emailManager.stopEmailRetrieval();
+
+        if (emailRetrievalWorker != null && !emailRetrievalWorker.isDone()) {
+            emailRetrievalWorker.cancel(true);
+        }
+    }
+
 }
--- src/Swing/EmailManager.java
+++ src/Swing/EmailManager.java
@@ -1,27 +1,74 @@
 package Swing;
 
-import javax.mail.Session;
+import javax.mail.*;
+import javax.mail.search.AndTerm;
+import javax.mail.search.FlagTerm;
+import javax.mail.search.ReceivedDateTerm;
+import javax.mail.search.SearchTerm;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Properties;
 
 
 public class EmailManager {
-    private final String username;
-    private final String password;
-    private final Session session;
-    private boolean stopListening = false;
+    private boolean stopRetrieval = false;
+    public void handleEmailRetrieval(String host, String username, String password) {
+        Properties properties = new Properties();
+        properties.put("mail.store.protocol", "imaps");
+        properties.put("mail.imap.host", "imap.gmail.com");
+        properties.put("mail.imap.port", "993");
 
-    public EmailManager(String username, String password) {
-        this.username = username;
-        this.password = password;
+        try {
+            Session session = Session.getInstance(properties);
+            Store store = session.getStore();
+            store.connect(host, username, password);
 
-        Properties props = new Properties();
-        props.put("mail.store.protocol", "imaps");
-        props.put("mail.imaps.host", "your-imap-host");
-        props.put("mail.imaps.port", "993");
+            Folder inbox = store.getFolder("INBOX");
+            inbox.open(Folder.READ_ONLY);
 
-        session = Session.getDefaultInstance(props);
+            SearchTerm searchTerm = getSearchTerm();
+
+            Message[] messages = inbox.search(searchTerm);
+
+            for (Message message : messages) {
+                if (Thread.interrupted() || stopRetrieval) {
+                    return;
+                }
+
+                Address[] fromAddresses = message.getFrom();
+                String sender = (fromAddresses != null && fromAddresses.length > 0)
+                        ? fromAddresses[0].toString()
+                        : "Unknown Sender";
+
+                System.out.println("DATE: " + message.getReceivedDate() + "Subject: " + message.getSubject() + "Sender: " + sender);
+            }
+
+            inbox.close(false);
+            store.close();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 
+    private static SearchTerm getSearchTerm() {
+        Calendar cal = Calendar.getInstance();
+        int currentYear = cal.get(Calendar.YEAR);
+        cal.set(currentYear, Calendar.DECEMBER, 1, 0, 0, 0);    //Poruke novije od 01.12.2023.
+        Date fromDate = cal.getTime();
+        ReceivedDateTerm receivedDateTerm = new ReceivedDateTerm(ReceivedDateTerm.GE, fromDate);
 
+        Flags seenFlag = new Flags(Flags.Flag.SEEN);
+        FlagTerm unseenFlagTerm = new FlagTerm(seenFlag, false);
 
+        SearchTerm searchTerm = new AndTerm(receivedDateTerm, unseenFlagTerm);
+        return searchTerm;
+    }
+
+    public void stopEmailRetrieval() {
+        stopRetrieval = true;
+    }
+
+    public void resetStopRetrieval() {
+        stopRetrieval = false;
+    }
 }
